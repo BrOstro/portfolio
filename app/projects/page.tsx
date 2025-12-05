23bf4548e26b271554d75016f6a5a1c299d43324
Advistane <Advistane@gmail.com>
2025-12-04 19:32:44 -0600

    Updated next js

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -2,7 +2,7 @@ import ProjectsPage from '@/components/page/projects-page';
 import type {Metadata} from 'next';
 
 export const metadata: Metadata = {
-	title: 'Projects - Brendan Ostrom',
+	title: 'Brendan Ostrom | Projects',
 	description: 'A collection of projects by Brendan Ostrom, showcasing his skills in full-stack development, AI/ML, and modern web technologies.',
 };
 
